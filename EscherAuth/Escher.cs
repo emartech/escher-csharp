ef92e909cd26b40ff28f0922b84ff7d5ab7ef48d
Tibor Toth <toth.t11@gmail.com>
2015-10-20 19:21:16 +0200

    initial commit

---
+++ EscherAuth/Escher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscherAuth
+{
+    public class Escher
+    {
+        public string Hello()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ EscherAuthTests/EscherTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using EscherAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscherAuthTests
+{
+    [TestFixture()]
+    public class EscherTests
+    {
+        [Test()]
+        public void HelloTest()
+        {
+            Assert.AreEqual("Hello World", new Escher().Hello());
+        }
+    }
+}
